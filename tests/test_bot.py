51903a36ef8fd29097cc02846a20efc619c22b1e
Hobson Lane <github@totalgood.com>
2017-08-01 09:45:42 -0700

    test_bot.py

---
+++ tests/test_bot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from civicu_app.bot import recognize_greeting
+
+__author__ = "Hobson Lane"
+__copyright__ = "Hobson Lane"
+__license__ = "mit"
+
+
+def test_recognize_greeting():
+    assert recognize_greeting('Hi') == True
+    assert recognize_greeting('Yo') == False
+    assert recognize_greeting('') == False
+    # with pytest.raises(AssertionError):
+    #     fib(-10)
