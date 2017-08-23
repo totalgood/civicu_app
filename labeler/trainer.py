cce2bcf1fae9540a6e74363e4276fd9e2e9b9d1f
Hobson Lane <github@totalgood.com>
2017-08-22 22:18:15 -0700

    start on a neural net based on a keras.blog post

--- labeler/trainer.py
+++ labeler/trainer.py
@@ -1,9 +1,24 @@
+""" CNN 16-layer neural net that generalizes well from 1000 cat images
+
+- uses the bottleneck features of a pre-trained VGG16 network
+- customizes the top layers of the pre-trained VGG16 network
+
+References:
+  [keras blog](https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html)
+  [code for keras blog](https://gist.github.com/fchollet/f35fbc80e066a49d65f1688a7e99f069)
+  [weights for pretrained VGG16](https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3)
+  [more labeled images](https://www.flickr.com/services/api/)
+  [cats and dogs images on kaggle](https://www.kaggle.com/c/dogs-vs-cats/data)
+  [image net images of more objects](http://image-net.org/download)
+"""
 import os
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import load_img, img_to_array  # , array_to_img
 
-from labeler_site.settings import BASE_DIR
+import labeler_site.settings
+
+BASE_DIR = labeler_site.settings.BASE_DIR
 
 
 # datagen = ImageDataGenerator(
