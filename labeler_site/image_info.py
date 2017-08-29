625f5dd80bf18bb083a6a5d73743a227002917fd
Hobson Lane <github@totalgood.com>
2017-08-28 20:28:08 -0700

    image_info script runs on glob star and streams output to stdout

--- labeler_site/image_info.py
+++ labeler_site/image_info.py
@@ -9,6 +9,7 @@ import PIL.Image
 from PIL.ExifTags import TAGS as tag_num2name
 from PIL.ExifTags import GPSTAGS as gpstag_num2name
 
+from pprint import pprint
 # `labeler_site` must be a python package installed in your environment (virtualenv)
 # OR "install" it manually before running this: `export PYTHONPATH=$PYTHONPATH:/path/to/labeler_site_basedir/`
 
@@ -19,7 +20,8 @@ django.setup()
 def get_exif(image_path=os.path.join(settings.BASE_DIR, 'labeler', 'data', 'SUNP0254.jpg')):
     """ Extract Exif header information from an image file and return it as a `dict` with informative keys
 
-
+    >>> get_exif()
+    [{...}]
     """
     img = PIL.Image.open(image_path)
     exif_data = img._getexif()
@@ -42,55 +44,14 @@ def get_exif(image_path=os.path.join(settings.BASE_DIR, 'labeler', 'data', 'SUNP
 
 
 def main(args):
-    if len(args) > 0:
-        exif_data = get_exif(' '.join(args))
-    else:
-        exif_data = get_exif()
-    return exif_data
+    return [get_exif(arg) for arg in args]
 
 
 def run():
-    return main(sys.argv[1:])
+    exif_data = main(sys.argv[1:])
+    pprint(exif_data)
 
 
 if __name__ == '__main__':
-    run()
-    # pprint(exif_data)
-    # {271: 'Icatch',
-    #  272: 'WF121',
-    #  282: (240, 1),
-    #  283: (240, 1),
-    #  296: 2,
-    #  305: 'Adobe Photoshop Lightroom 4.4 (Windows)',
-    #  306: '2016:07:31 14:16:14',
-    #  33434: (1, 60),
-    #  33437: (32, 10),
-    #  34665: 200,
-    #  34850: 2,
-    #  34855: 100,
-    #  36864: b'0230',
-    #  36867: '2014:04:27 17:40:29',
-    #  36868: '2014:04:27 17:40:29',
-    #  37377: (5906891, 1000000),
-    #  37378: (3356144, 1000000),
-    #  37379: (-5000, 1000),
-    #  37380: (0, 10),
-    #  37381: (3, 2),
-    #  37383: 4,
-    #  37384: 0,
-    #  37385: 1,
-    #  37386: (82, 11),
-    #  37396: (1536, 1152, 3072, 2304),
-    #  41728: b'\x03',
-    #  41729: b'\x01',
-    #  41985: 0,
-    #  41986: 0,
-    #  41987: 0,
-    #  41988: (0, 1),
-    #  41990: 0,
-    #  41991: 0,
-    #  41992: 0,
-    #  41993: 0,
-    #  41994: 0,
-    #  41996: 0}
-    print(json.dumps(exif_data, indent=2))
+    exif_data = run()
+    pprint(exif_data)
